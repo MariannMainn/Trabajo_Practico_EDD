ff40d2a431bd0c9c6320fceebafe28a1d4d16f9a
solaricande <solaricandela11@gmail.com>
2025-09-20 15:54:32 -0300

    correo_servidor.py

--- modulos/correo_servidor.py
+++ modulos/correo_servidor.py
@@ -1,8 +1,14 @@
-#clase ServidorCorreo representa la interfaz que usaran las demas clases                           
-class ServidorCorreo:
+#clase servidorCorreo
+class servidorCorreo:
+        def __init__(self):
+        self.user = [] 
+        self.mensajes = []
+    #Metodos 
+        def registrarUser(self, user):
+        self._users.append(users)
+
+        def enviarMensaje(self, destinatario):
+        self._buscarUser(destinatario)
         
-    def enviar_mensaje(self):
-        pass
-    
-    def obtener_Mensajes (self):
-        pass
+        def _buscarUser(self, correo):
+        self._users
